18439466b8fb73c1ccadb5effe35160dcc328804
James Avery <j-a-avery@users.noreply.github.com>
2022-03-31 18:01:10 -0500

    Added tool to convert word list to TS string[].

---
+++ tools/wordlistToTs.py
@@ -0,0 +1,18 @@
+import json;
+import os
+import sys 
+
+with open("./data/answers.txt", 'r') as f:
+  words = [w.strip() for w in f.readlines()]
+
+numwords = len(words)
+
+words = (
+  "export default const words: " + 
+  f"string[] = {json.dumps(words)};"
+)
+
+with open("./src/wordlist.ts", "w") as f:
+  f.write(words)
+
+print(f"Wrote {numwords} words to {'./src/wordlist.js'}.")
